b33c39cd6777095d900eaabcab0e229c7354a893
William Chan <you@example.com>
2020-03-23 10:29:16 -0400

    google test screening

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { NumberStream } from './numberStream';
+import { toArray } from 'rxjs/operators';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'test'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('test');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
+  });
+
+  it('should emit true for numbers', () => {
+    const testInput = from(['abc', 3, 'def', 1.5, 'xyz', 200]);
+
+    const testStream = new NumberStream(testInput);
+    testStream.isNumber().pipe(toArray()).subscribe(output => {
+      console.log(output);
+      expect(output).toEqual([false, true, false, true, false, true])
+    })
+  });
+});
---
+++ src/app/numberStream.ts
@@ -0,0 +1,25 @@
+// Comment Mandatory prescreen questions:
+// 1. Interface Using Typescript, define an interface for a class that has one public property `valueStream` and one public member function `isNumber`. `valueStream` is an Observable that emits either a string or number. `isNumber` has no arguments and returns an Observable that emits a boolean.
+// 2. Implementation Implement the interface defined in (1). The `valueStream` property should be initialized as a constructor parameter. The Observable returned by `isNumber` should emit true if `valueStream` emits a number, otherwise it should emit false.
+// 3. Testing Write a test case for the implementation in (2).
+
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
+
+interface INumberStream {
+  valueStream: Observable<string|number>;
+  isNumber: () => Observable<boolean>
+}
+
+export class NumberStream implements INumberStream {
+  valueStream: Observable<string|number>;
+  constructor (vStream: Observable<string|number>) {
+    this.valueStream = vStream;
+  }
+
+  isNumber() {
+    return this.valueStream.pipe(
+      map(v => typeof v !== 'string')
+    )
+  }
+}
